39a16eb488c4973c06f056b9e8e7d37e8bbd052e
Chris Facer <chris.facer@q-researchsoftware.com>
2016-06-21 09:52:25 +1000

    Updated pcaPhoneData to include a missing variable

--- R/data-raw/pcaphonetestdata.R
+++ R/data-raw/pcaphonetestdata.R
@@ -2,7 +2,7 @@
 # Q ('Data Preparation1.Q') and then saved out as an SPSS file, which is turned into an R data file here.
 
 
-temp <- foreign::read.spss(system.file("extdata", "pcaPhoneTestData.sav", package = "flipData"),
+temp <- foreign::read.spss(system.file("extdata", "pcaPhoneTestData.sav", package = "flipExampleData"),
                            to.data.frame = TRUE, use.value.labels = FALSE)
 pcaPhoneTestData <- list(data.set = temp[,match("q2320_1", names(temp)):match("q2320_25", names(temp))],
                   data.set.original =  temp[,match("q23a", names(temp)):match("q23y", names(temp))],
