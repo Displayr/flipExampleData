9021d6628f0787181ad05f2c02219b6bb811663a
TimBock <tim.bock@numbers.net.au>
2016-08-30 08:51:54 +1000

    Adding label attribute to phone

--- R/data-raw/phone.R
+++ R/data-raw/phone.R
@@ -1,3 +1,4 @@
-phone <- foreign::read.spss(system.file("extdata", "Phone.sav", package = "flipExampleData"))
+phone <- foreign::read.spss(system.file("extdata", "Phone.sav", package = "flipExampleData"), to.data.frame = TRUE)
+phone <- TidySPSS(phone)
 devtools::use_data(phone, internal = FALSE, overwrite = TRUE)
 
