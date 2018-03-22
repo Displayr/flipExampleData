68b47da45a7b0ec7c91fa5167e395e2da3dd6dad
jakehoare <jake_hoare@hotmail.com>
2018-03-22 12:05:53 +1100

    Add files via upload

---
+++ adult2000.R
@@ -0,0 +1,4 @@
+adult.2000 <- read.csv(file.path("inst", "extdata", "adult_2000.csv"),
+    stringsAsFactors = TRUE, encoding = "UTF-8")
+
+devtools::use_data(adult.2000, internal = FALSE, overwrite = TRUE)
