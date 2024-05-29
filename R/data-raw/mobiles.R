4e23f96600290062a7818dcf46653238faa26dbf
jrwishart <justin.wishart@displayr.com>
2024-05-29 13:10:01 +1000

    fixup! Use new filename

--- R/data-raw/mobiles.R
+++ R/data-raw/mobiles.R
@@ -3,5 +3,5 @@ require(usethis)
 
 file <- system.file("extdata", "Mobiles_feedback_4_weeks.sav", package = "flipExampleData")
 mobiles.feedback.4.weeks <- foreign::read.spss(file, to.data.frame = TRUE, reencode = TRUE, max.value.labels = 20)
-mobiles.feedback.4.weeks <- TidySPSS(mobiles.feedback)
+mobiles.feedback.4.weeks <- TidySPSS(mobiles.feedback.4.weeks)
 use_data(mobiles.feedback.4.weeks, internal = FALSE, overwrite = TRUE)
